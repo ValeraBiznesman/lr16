9706a6edadaeeb75b0f4bd42dd876f8c0cd27aab
valera <artemart2009@gmail.com>
2026-06-17 21:50:40 +0300

    second commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Manufacturer, Product, Cart, CartItem
+admin.site.register(Category)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'total_price')
 
-# Register your models here.
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'item_total_price')
