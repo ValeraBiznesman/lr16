92a78ea483ec7757915816d089f2e5697a02dfd7
valera <artemart2009@gmail.com>
2026-06-17 22:30:12 +0300

    four commit

--- shop/urls.py
+++ shop/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/update/<int:item_id>/', views.update_cart, name='update_cart'),
     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    
-    # Авторизация
+    path('checkout/', views.checkout, name='checkout'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('checkout/success/', views.checkout_success, name='checkout_success'),
+    
 ]
